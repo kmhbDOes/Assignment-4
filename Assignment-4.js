bce2dded7c9cd0d4ac69ae536168caa15c1a7234
kmhbDOes <kmhbipul@gmail.com>
2023-02-06 22:19:44 +0600

    Problem 1 done

--- Assignment-4.js
+++ Assignment-4.js
@@ -1,1 +1,23 @@
-console.log('wc');
+// Problem 1: Let’s play a mind game ******
+
+function mindGame(mindGameInput) {
+    let mindGameOutput = (mindGameInput * 3 + 10) / 2 - 5;
+
+    if (typeof mindGameInput == 'string') {
+        return "Please Enter a valid number:"
+    }
+
+    return mindGameOutput;
+}
+let mindGameResult = mindGame(5);
+console.log(mindGameResult);
+
+
+
+
+// Problem 2: Finding even or odd
+
+function evenOdd(string) {
+
+}
+
