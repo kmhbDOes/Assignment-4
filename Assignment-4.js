11cce2fcce5f905ed04d4b05ff5e9a93e782ec01
kmhbDOes <kmhbipul@gmail.com>
2023-02-07 14:28:17 +0600

    code updated

--- Assignment-4.js
+++ Assignment-4.js
@@ -1,5 +1,6 @@
 // Problem 1: Let’s play a mind game ******
 
+//  function mindGame here has one parameter named mindGamInput which takes a number and we make a calculation based on the input with mindGameOutput having the below formula and thus we return mindGameOutput
 function mindGame(mindGameInput) {
     let mindGameOutput = (mindGameInput * 3 + 10) / 2 - 5;
 
@@ -17,19 +18,21 @@ console.log(mindGameResult);
 
 // Problem 2: Finding even or odd
 
+// function evenOdd takes a string as input and counts the length and returns based on the count that if it's even or odd
+
 function evenOdd(string) {
     for (let i = 0; i < string.length; i++) {
         if (string.length % 2 == 0) {
-            console.log(string, "is Even");
+            return "even";
         }
         else {
-            return "Odd";
+            return "odd";
         }
-        return string;
     }
+    return string;
 }
 
-let checkEvenOdd = evenOdd("Pheroo");
+let checkEvenOdd = evenOdd("Phero");
 console.log(checkEvenOdd);
 
 
@@ -37,9 +40,14 @@ console.log(checkEvenOdd);
 
 // Problem 3: Is Less or Greater than seven
 
+// function isLGSeven takes a number parameter as input, If that number is less than 7 then return difference between that number and 7 otherwise double the input.
+
 function isLGSeven(isLGSevenInput) {
     if (isLGSevenInput < 7) {
         return (isLGSevenInput - 7);
+    }
+    if (typeof isLGSevenInput == 'string') {
+        return "Enter Valid Number";
     }
     return isLGSevenInput * 2;
 
@@ -51,6 +59,8 @@ console.log(lr);
 
 
 // Problem 4: Finding Bad data
+
+// function findingBadData takes a array as input
 
 function findingBadData(inputArray) {
     let negativeCount = 0;
